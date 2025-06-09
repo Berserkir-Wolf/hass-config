a2c5ef7ecc4ce59c0abb3a731ae6094dcf249b62
Dyson Parkes <dyson@berserkir.net>
2025-06-09 15:52:30 +1200

    update audiobookshelf

--- custom_components/audiobookshelf/const.py
+++ custom_components/audiobookshelf/const.py
@@ -2,9 +2,6 @@
 
 from homeassistant.const import Platform
 
-VERSION = "v0.2.3"
-ISSUE_URL = "https://github.com/wolffshots/hass-audiobookshelf/issues"
+VERSION = "v0.2.5"
 DOMAIN = "audiobookshelf"
 PLATFORMS: list[Platform] = [Platform.SENSOR]
-HTTP_OK = 200
-HTTP_AUTH_FAILURE = 401
